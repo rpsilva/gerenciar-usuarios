bf50808fc8267f9bbd9eb6199b8ab1a85b54f53d
Rodrigo <rdgo_silva@hotmail.com>
2025-09-15 00:21:34 -0300

    Atualização do projeto com utilização de objeto

--- src/gerenciarUsuarios.js
+++ src/gerenciarUsuarios.js
@@ -1,4 +1,25 @@
-const usuarios = ['matheus', 'marcos', 'lucas', 'joão'];
+//const usuarios = ['matheus', 'marcos', 'lucas', 'joão'];
+
+const usuarios =
+ [
+		    {
+			nome: 'Matheus',
+			email: 'matheus@teste.com',
+	        },
+            {
+			nome: 'Marcos',
+			email: 'matheus@teste.com',
+	        },
+            {
+			nome: 'Lucas',
+			email: 'matheus@teste.com',
+	        },
+            {
+			nome: 'João',
+			email: 'matheus@teste.com',
+	        }
+]
+
 
 function retornarUsuarios () {
     return usuarios;
@@ -7,9 +28,16 @@ function retornarUsuarios () {
 //no js se temos algo fora do escopo, ele pode ser utilizado dentro do escopo
 //ex.: o const usuarios pode ser utilizado dentro do escopo {}
 
+/** 
 function adicionarNovoUsuario(nome){
     usuarios.push(nome);
+} */
+
+
+function adicionarNovoUsuario(usuario){
+    usuarios.push(usuario);
 }
+
 
 module.exports = {
     retornarUsuarios,
--- test/gerenciarUsuarios.test.js
+++ test/gerenciarUsuarios.test.js
@@ -4,13 +4,25 @@ const {expect} = require('chai');
 describe('Testar as funções de Gestão de Usuários', function() {
     it('Validar que posso adicionar um novo nome de usuário na lista', function() {
         //1. Adicioanr um novo nome na lista de usuários
-        adicionarNovoUsuario('Maria');
+       // adicionarNovoUsuario('Maria');
+       adicionarNovoUsuario({
+            nome: 'Maria',
+            email:'maria@teste.com'
+       });
+
+
         //2. Retonar a lista de usuários na caixa lista de usuários
         const listaDeUsuarios = retornarUsuarios();
 
         //3. Comparar se o novo nome está no fim da lista de usuários
-        expect(listaDeUsuarios.at(-1)).to.equal('Maria'); //(-1) traz o ultimo item da lista
+        //expect(listaDeUsuarios.at(-1)).to.equal('Maria'); //(-1) traz o ultimo item da lista
+
+        expect(listaDeUsuarios.at(-1.)).to.eql({
+            nome: 'Maria',
+            email: 'maria@teste.com'
+
     });
 
+  });
 
 });
